4ee75f375ec4a05be280401dd2e03d2c29029527
haihua.gu <haihua.gu@gaiaworks.cn>
2020-03-16 09:44:17 +0800

    update

--- src/main/java/com/ghh/sample/controller/ApplicationController.java
+++ src/main/java/com/ghh/sample/controller/ApplicationController.java
@@ -4,16 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class ApplicationController {
     private final static Logger logger = LoggerFactory.getLogger(ApplicationController.class);
 
-    @GetMapping("/test")
+    @GetMapping("/test1")
     public ModelAndView test(ModelAndView mv) {
         mv.setViewName("test");
         logger.info("---------test 123 logger");
         return mv;
     }
+
+    @GetMapping("/test")
+    public String testApi() {
+        logger.info("process request {}", System.currentTimeMillis());
+        return "success";
+    }
+
 }
